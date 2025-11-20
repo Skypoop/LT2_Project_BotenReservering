d8f00d374d57d4d39c5ef8adff761a8d9c8509e6
Fabian <s1214550@student.windesheim.nl>
2025-11-20 15:21:49 +0100

    fix: revert test WeatherAPI in UI

--- ProjectBotenReservering.App/ViewModels/HomePageViewModel.cs
+++ ProjectBotenReservering.App/ViewModels/HomePageViewModel.cs
@@ -1,25 +1,6 @@
-using CommunityToolkit.Maui.Alerts;
-using CommunityToolkit.Maui.Core;
-using CommunityToolkit.Mvvm.Input;
-using ProjectBotenReservering.Core.Interfaces;
-using System.Text.Json;
-
 namespace ProjectBotenReservering.App.ViewModels;
 
-public partial class HomePageViewModel : BaseViewModel
+public class HomePageViewModel : BaseViewModel
 {
-    private readonly IWeatherService _weatherService;
 
-    public HomePageViewModel(IWeatherService weatherService)
-    {
-        _weatherService = weatherService;
-    }
-
-    [RelayCommand]
-    public async Task GetWeather()
-    {
-        int windforce = await _weatherService.GetWeatherAsync();
-        await Shell.Current.DisplayAlert("Weather Report", $"De windkracht is {windforce} met rang N is het mogelijk dat jouw reservering wordt geannuleerd", "OK");
-
-    }
-}
+};
