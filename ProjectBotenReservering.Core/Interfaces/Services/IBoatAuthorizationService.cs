6c86bc41190c5b171d089fbbc283876e06d28f23
ernestoheres <ernestoheres09@gmail.com>
2025-11-24 19:12:54 +0100

    fix: new overloaded method was not updated in interface

--- ProjectBotenReservering.Core/Interfaces/Services/IBoatAuthorizationService.cs
+++ ProjectBotenReservering.Core/Interfaces/Services/IBoatAuthorizationService.cs
@@ -5,7 +5,7 @@ namespace ProjectBotenReservering.Core.Interfaces.Services;
 public interface IBoatAuthorizationService
 {
     bool IsAuthorized(BoatType boatType, int boatLevel);
-    bool ClientIsAuthorized(BoatType boatType, int boatLevel, Client client);
+    bool IsAuthorized(BoatType boatType, int boatLevel, Client client);
 
     IEnumerable<T> FilterAuthorized<T>(IEnumerable<T> items, Func<T, BoatType> boatTypeSelector, Func<T, int> boatLevelSelector);
 }
