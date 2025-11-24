ae7c8e8622772efd7a381d7b990f405b129084b1
ernestoheres <ernestoheres09@gmail.com>
2025-11-24 16:09:00 +0100

    feat: scaffold service and implement maxReservationLength

---
+++ ProjectBotenReservering.Core/Constants/ReservationRules.cs
@@ -0,0 +1,7 @@
+namespace ProjectBotenReservering.Core.Constants;
+
+public class ReservationRules
+{
+    public const int MaxReservationLength = 2;
+    public const int MaxDaysBeforeReservation = 2;
+}
--- ProjectBotenReservering.Core/Services/ReservationService.cs
+++ ProjectBotenReservering.Core/Services/ReservationService.cs
@@ -1,6 +1,38 @@
+using ProjectBotenReservering.Core.Interfaces.Repositories;
+using ProjectBotenReservering.Core.Interfaces.Services;
+using ProjectBotenReservering.Core.Models;
+using ProjectBotenReservering.Core.Constants;
+
 namespace ProjectBotenReservering.Core.Services;
 
 public class ReservationService
 {
+    private readonly IReservationRepository _reservationService;
+    private readonly IBoatAuthorizationService _boatAuthorizationService;
+    public ReservationService(IReservationRepository reservationRepository, IBoatAuthorizationService boatAuthorizationService)
+    {
+        this._reservationService = reservationRepository;
+        this._boatAuthorizationService = boatAuthorizationService;
+        
+    }
     
+    public bool IsBookingWithinAllowedReservationTime(DateTime startTime, DateTime endTime)
+    {
+        return true;
+    }
+
+    public bool IsValidReservationLength(DateTime startTime, DateTime endTime)
+    {
+        TimeSpan timeDifference = endTime - startTime;
+        if (timeDifference.Hours >= ReservationRules.MaxReservationLength)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public bool IsReservationTimeFree(DateTime startTime, DateTime endTime)
+    {
+        return true;
+    }
 }
