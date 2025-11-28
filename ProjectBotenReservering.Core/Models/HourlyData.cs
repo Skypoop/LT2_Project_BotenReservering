b08e5a7dd3fac19e9663ce7c195258efb88310cb
Joppe20003 <s1211156@student.windesheim.nl>
2025-11-28 11:05:39 +0100

    Refactor weather data classes and JSON serialization
    Removed `using System.Collections.Generic;` from `WeatherData.cs` and moved `CurrentWeather` and `HourlyData` classes to their own files. Added properties for `Windspeed`, `WindSpeed10m`, and `Time` with required lists. Included `JsonPropertyName` attributes for proper JSON serialization.

---
+++ ProjectBotenReservering.Core/Models/HourlyData.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ProjectBotenReservering.Core.Models
+{
+    public class HourlyData
+    {
+        [JsonPropertyName("wind_speed_10m")]
+        public required List<decimal> WindSpeed10m { get; set; }
+
+        // Als je ook tijden wilt opslaan:
+        [JsonPropertyName("time")]
+        public required List<string> Time { get; set; }
+    }
+}
--- ProjectBotenReservering.Core/Models/WeatherData.cs
+++ ProjectBotenReservering.Core/Models/WeatherData.cs
@@ -1,5 +1,4 @@
 ﻿using System.Text.Json.Serialization;
-using System.Collections.Generic;
 
 namespace ProjectBotenReservering.Core.Models
 {
@@ -10,21 +9,5 @@ namespace ProjectBotenReservering.Core.Models
 
         [JsonPropertyName("hourly")]
         public required HourlyData Hourly { get; set; }
-    }
-
-    public class CurrentWeather
-    {
-        [JsonPropertyName("windspeed")]
-        public decimal Windspeed { get; set; }
-    }
-
-    public class HourlyData
-    {
-        [JsonPropertyName("wind_speed_10m")]
-        public required List<decimal> WindSpeed10m { get; set; }
-
-        // Als je ook tijden wilt opslaan:
-        [JsonPropertyName("time")]
-        public required List<string> Time { get; set; }
     }
 }
