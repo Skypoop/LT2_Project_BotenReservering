e847f4a363b121e267a4d94a5c4f8e010ad51bc1
Tein <s1213197@student.windesheim.nl>
2025-12-06 14:57:16 +0100

    test: backfill unit tests for BoatTypeService

---
+++ TestCore/Services/BoatTypeServiceTests.cs
@@ -0,0 +1,67 @@
+using Moq;
+using NUnit.Framework;
+using FluentAssertions;
+using ProjectBotenReservering.Core.Services;
+using ProjectBotenReservering.Core.Interfaces.Services;
+using ProjectBotenReservering.Core.Interfaces.Repositories;
+using ProjectBotenReservering.Core.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace TestCore.Services;
+
+[TestFixture]
+public class BoatTypeServiceTests
+{
+    private Mock<IBoatRepository> _mockBoatRepository;
+    private Mock<IBoatAuthorizationService> _mockBoatAuthorizationService;
+    private BoatTypeService _service;
+
+    [SetUp]
+    public void Setup()
+    {
+        _mockBoatRepository = new Mock<IBoatRepository>();
+        _mockBoatAuthorizationService = new Mock<IBoatAuthorizationService>();
+        _service = new BoatTypeService(_mockBoatRepository.Object, _mockBoatAuthorizationService.Object);
+    }
+
+    [Test]
+    public void GetBoatTypes_ReturnsOnlyAuthorizedBoats()
+    {
+        // Arrange
+        List<Boat> allBoats = new List<Boat>
+        {
+            new Boat("Boat1", true, 1, 2, BoatType.S, 80, true, "Club", 1),
+            new Boat("Boat2", true, 1, 3, BoatType.S, 80, true, "Club", 2)
+        };
+
+        List<Boat> authorizedBoats = new List<Boat>
+        {
+            allBoats[0]
+        };
+
+        _mockBoatRepository.Setup(r => r.GetAll()).Returns(allBoats);
+        
+        // Setup FilterAuthorized to return the authorized list
+        _mockBoatAuthorizationService
+            .Setup(s => s.FilterAuthorized(
+                It.IsAny<IEnumerable<Boat>>(), 
+                It.IsAny<Func<Boat, BoatType>>(), 
+                It.IsAny<Func<Boat, int>>()))
+            .Returns(authorizedBoats);
+
+        // Act
+        List<BoatTypeUiItem> result = _service.GetBoatTypes();
+
+        // Assert
+        result.Should().HaveCount(1);
+        result.First().Name.Should().Be("Boat1");
+        
+        _mockBoatRepository.Verify(r => r.GetAll(), Times.Once);
+        _mockBoatAuthorizationService.Verify(s => s.FilterAuthorized(
+            It.IsAny<IEnumerable<Boat>>(), 
+            It.IsAny<Func<Boat, BoatType>>(), 
+            It.IsAny<Func<Boat, int>>()), Times.Once);
+    }
+}
