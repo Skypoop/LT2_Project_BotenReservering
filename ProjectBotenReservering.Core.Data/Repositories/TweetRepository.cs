dd43e2bc43c85ba18b590715e3d570b6ee16843a
ernestoheres <s1217658@student.windesheim.nl>
2025-12-17 01:26:26 +0100

    feat-UC7-US31: make interface and add files for repository and rest client

---
+++ ProjectBotenReservering.Core.Data/Repositories/TweetRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjectBotenReservering.Core.Data.Repositories;
+
+using System;
+using System.Threading.Tasks;
+using ProjectBotenReservering.Core.Data.RestClients;
+using ProjectBotenReservering.Core.Interfaces.Repositories;
+public class TweetRepository
+{
+    
+}
+
---
+++ ProjectBotenReservering.Core/Interfaces/Repositories/ITweetRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectBotenReservering.Core.Interfaces.Repositories
+{
+    public interface ITweetRepository
+    {
+        // todo find type for postmedia
+        Task<string> PostMedia(string file);
+        Task<string> PostTweet(string prompt);
+    }
+}
